81f521317485c884c482dc6371b9411a9525635c
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-11 12:22:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-Velkommen!
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def main():
+
+    # Loading dataset.
+    data = sns.load_dataset("penguins")
+
+    print(data.head())
+    # print(data.info())
+    # print(data.describe())
+    # print(data.isna().sum())
+
+
+if __name__ == "__main__":
+    main()
