1c95126221c216ac26ded9289ee4c819b4df5990
Mikkel Dollas <mikkeldollaskontakt@gmail.com>
2025-11-12 08:35:09 +0100

    Updated main.py again

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
     # Loading dataset.
     data = pd.read_csv(CSV_PATH)
 
-    features = ["",]
-    target = "species"
-    data = data[features + [target]]
-    print(data.head())
+    # features = ["",]
+    # target = "species"
+    # data = data[features + [target]]
+    # print(data.head())
 
     data = data.dropna()
     data = pd.get_dummies(data, drop_first=True)
